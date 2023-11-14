8f43f457e3fda97a19ed366e25ede0b3c2ff2cb6
Subhadip <112926665+Subhadip-20@users.noreply.github.com>
2023-11-14 05:00:07 +0000

    Library Management System added

---
+++ src/LibraySystem/Book.java
@@ -0,0 +1,16 @@
+package LibrarySystem;
+
+public class Book {
+    String title;
+        String author;
+            String category;
+                boolean available;
+
+                    public Book(String title, String author, String category) {
+                            this.title = title;
+                                    this.author = author;
+                                            this.category = category;
+                                                    this.available = true;
+                                                        }
+                                                        }
+                                                        
